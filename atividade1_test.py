b170bdfb28d966454a241cff7942a0e5ddb351c3
ebenezerandrade <ebenezer.andrade1@gmail.com>
2016-09-07 17:40:31 -0300

    Create method for test local_image

--- atividade1_test.py
+++ atividade1_test.py
@@ -4,9 +4,9 @@ import cv2
 class TestProcessFaces(unittest.TestCase):
     def test_normal_directory_local(self):
         from atividade1 import processFaces
-        local_image = raw_imput("Insert directory") 
-        assert processFaces(local_image) == cv2.inshow('image', local_image)
-        self.assertEqual(processFace(local_image),cv2.inshow('image'),local_image)
+        local_image = raw_input("Insert directory") 
+        assert processFaces(local_image) == cv2.waitKey()
+        self.assertEqual(processFaces(local_image),cv2.waitKey())
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
