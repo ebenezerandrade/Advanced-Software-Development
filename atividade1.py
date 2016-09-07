4c8b3c884474d4af940bf106bfc7ee53628eb1d7
ebenezerandrade <ebenezer.andrade1@gmail.com>
2016-09-07 13:49:57 -0300

    create method for processing faces

--- atividade1.py
+++ atividade1.py
@@ -1,7 +1,7 @@
-#Desenvolvimento Avancado de Software
-#Autor: Ebenezer Andrade da Silva 12/0060213
-#Atividade 1 - Algoritmo parcial do exercicio
-#resolvendo apenas a deteccao das faces
+# Desenvolvimento Avancado de Software
+# Autores: Ebenezer Andrade e Omar Junior
+# Atividade 2 - Pair Programming
+# Algoritmo solucionando apenas a deteccao das faces
 
 import urllib
 import cv2
@@ -20,74 +20,22 @@ def menu():
     return select
 
 
-def drawRectangle():
-
-    directory_classificator = '/home/ebenezer/DAS/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
-    local_image = raw_input('Entre com o caminho da imagem:')
-
-    #Directory with the classifier for the facesFrontais
-    faceCascade = cv2.CascadeClassifier(directory_classificator)
-    image = cv2.imread(local_image, -1)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    #Detect objects in the picture
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-
-    #Print amount faces found
-    print "Encontradas {0} faces!".format(len(faces))
-
-    #Draw retangle in around faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
 def main():
-
-    directory_classificator = '/home/ebenezer/DAS/opencv/data/haarcascades/haarcascade_frontalface_default.xml'
 
     while True:
         select = menu()
         if select == 1:
             print('Opcao 1 escolhida')
             local_image = raw_input('Entre com o caminho da imagem:')
-
-            #Directory with the classifier for the facesFrontais
-            faceCascade = cv2.CascadeClassifier(directory_classificator)
-            image = cv2.imread(local_image, -1)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            #Detect objects in the picture
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30),
-                flags = cv2.CASCADE_SCALE_IMAGE
-            )
-
-            #Print amount faces found
-            print "Encontradas {0} faces!".format(len(faces))
-
-            #Draw retangle in around faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            cv2.imshow('imagem Local', image)
-            cv2.waitKey()
+            processFaces(local_image)
         elif select == 2:
-            #Request URL to user
+            # Request URL to user
             url = raw_input('Insere a URL:')
 
-            #Get content of URL
+            # Get content of URL
             url_response = urllib.urlopen(url)
 
-            #convert into a numpy array
+            # Convert into a numpy array
             image_array = np.array(bytearray(url_response.read()), dtype=np.uint8)
             imageCode = cv2.imdecode(image_array, -1)
 
@@ -96,76 +44,57 @@ def main():
 
             imageCode = file
 
-            #Directory with the classifier for the facesFrontais
-            faceCascade = cv2.CascadeClassifier(directory_classificator)
-            image = cv2.imread(imageCode)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            #Detect objects in the picture
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30),
-                flags = cv2.CASCADE_SCALE_IMAGE
-            )
-
-            #Print amount faces found
-            print "Encontradas {0} faces!".format(len(faces))
-
-            #Draw retangle in around faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            cv2.imshow('Imagem', image)
-            cv2.waitKey()
+            processFaces(imageCode)
         elif select == 3:
 
             camera_port = 0
-            #Initialize the camera on port 0 using Video Capture
+            # Initialize the camera on port 0 using Video Capture
             camera = cv2.VideoCapture(camera_port)
 
-            #capture a single image
+            # Capture a single image
             def get_image():
                 retval, im = camera.read()
                 return im
 
             print("Tirando a foto")
-            #stores obtained image
+            # Stores obtained image
             camera_capture = get_image()
             file = "/home/ebenezer/DAS/test_image.png"
 
             cv2.imwrite(file, camera_capture)
 
             fileFinal = file
-
-            #Directory with the classifier for the facesFrontais
-            faceCascade = cv2.CascadeClassifier('/home/ebenezer/DAS/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
-            image = cv2.imread(fileFinal)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            #Detect objects in the picture
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(20, 20),
-                flags = cv2.CASCADE_SCALE_IMAGE
-            )
-
-            #Print amount faces found
-            print "Encontradas {0} faces!".format(len(faces))
-
-            #Draw retangle in around faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            cv2.imwrite(fileFinal, image)
-            cv2.imshow('Imagem da Webcam', image)
-            cv2.waitKey()
+            processFaces(fileFinal)
         elif select == 4:
             exit()
         else:
             print('Opcao invalida tente novamente')
+
+
+def processFaces(object):
+    # Directory with the classifier for the facesFrontais
+    faceCascade = cv2.CascadeClassifier('/home/ebenezer/DAS/opencv/data/haarcascades/haarcascade_frontalface_default.xml')
+    image = cv2.imread(object)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect objects in the picture
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(20, 20),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Print amount faces found
+    print "Encontradas {0} faces!".format(len(faces))
+
+    # Draw retangle in around faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Imagem da Webcam', image)
+
+    return cv2.waitKey()
 
 main()
